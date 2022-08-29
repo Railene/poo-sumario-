4b4cc020e4305aabf7164a0126e6ab79b4d7cfac
Railene <rbds.ecp19@uea.edu.br>
2022-08-29 13:08:18 -0400

    primeiro commit

---
+++ src/application/Programa.java
@@ -0,0 +1,65 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidades.Client;
+import entidades.Ordem;
+import entidades.OrderItem;
+import entidades.Produto;
+import model.enums.OrderStatus;
+
+public class Programa {
+
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		System.out.println("Digite os dados do cliente:");
+		System.out.print("Nome: ");
+		String nome = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.next();
+		System.out.print("Data de Nascimento (DD/MM/AAAA): ");
+		Date dataNascimento =  sdf.parse(sc.next());
+		
+		Client client = new Client(nome, email, dataNascimento);
+		
+		System.out.println("Digite os dsdos do pedido:");
+		System.out.print("Status: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		Ordem order = new Ordem(new Date(), status, client);
+		
+		System.out.print("Quantos itens para esse pedido? ");
+		int n = sc.nextInt();
+		for (int i=1; i<=n; i++) {
+			System.out.println("Digite #" + i + " dados do item:");
+			System.out.print("Nome do Produto: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.print("Preço do produto: ");
+			double productPrice = sc.nextDouble();
+
+			Produto product = new Produto(productName, productPrice);
+
+			System.out.print("Quantidade: ");
+			int quantity = sc.nextInt();
+
+			OrderItem orderItem = new OrderItem(quantity, productPrice, product); 
+
+			order.addItem(orderItem);
+		}
+		
+		System.out.println();
+		System.out.println("Resumo do Pedido:");
+		System.out.println(order);
+		
+		sc.close();
+	}
+
+}
